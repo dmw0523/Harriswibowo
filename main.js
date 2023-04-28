065f3ec186e583b7fca55ae0ba1a6a931774b394
dmw0523 <dmw190815@gmail.com>
2023-04-28 13:31:28 +0700

    v1

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const header =document.querySelector("header");
+const tombol = document.querySelector(".toggle");
 
 window.addEventListener("scroll",function(){
 header.classList.toggle ("sticky", this.window.scrollY>0);
+tombol.classList.toggle ("tombol", this.window.scrollY>0);
 
 });
+
