5a1bd37b4dec56c5ef0738212fa7ffdec5f6f858
dmw0523 <dmw190815@gmail.com>
2023-04-28 15:33:46 +0700

    v1

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const header =document.querySelector("header");
-const tombol = document.querySelector(".toggle");
+const tombol = document.querySelector(".btn-menu");
 
 window.addEventListener("scroll",function(){
 header.classList.toggle ("sticky", this.window.scrollY>0);
 tombol.classList.toggle ("tombol", this.window.scrollY>0);
 
 });
+
+const menu= document.querySelector(".menu-icon");
+const menunav= document.querySelector(".menu-nav");
+
+menu.onclick = () => {
+
+    menu.classList.toggle('close');
+    menunav.classList.toggle('active');
+};
+
+
+window.onscroll = () => {
+
+ 
+    menunav.classList.remove('active');
+};
 
